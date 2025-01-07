965668aca89cc5615339d6378ec286abe4b8f491
AlyssaY <97510875+SatoshiUnit@users.noreply.github.com>
2025-01-08 07:26:15 +1100

    CurrencySwitchMadeEasy.py

--- Untitled.py
+++ Untitled.py
@@ -1,27 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
-The following demonstrates how to create 
-
-
-# In[1]:
-
 
 pip install -U pycoingecko
-
-
-# In[2]:
-
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from pycoingecko import CoinGeckoAPI
-
-
-# In[3]:
 
 
 cg = CoinGeckoAPI()
@@ -29,13 +11,8 @@ cg = CoinGeckoAPI()
 cg.ping()
 
 
-# In[4]:
-
-
 cg.get_price(ids=['bitcoin', 'litecoin', 'ethereum'], vs_currencies='usd')
 
-
-# In[8]:
 
 
 def change_currency():
@@ -62,12 +39,6 @@ def change_currency():
     
    
     button.click()
-
-    # Close the browser
-    driver.quit()
-
-
-# In[9]:
 
 
 change_currency('bitcoin')
